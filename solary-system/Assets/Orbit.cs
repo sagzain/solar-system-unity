8bfc4b31a9be98507e23cb20455261a62fe47bea
Samuel <samuel.gonzalez3@alu.uclm.es>
2021-11-22 21:03:16 +0100

    #unity basic orbit and rotation with cubes

---
+++ solary-system/Assets/Orbit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _orbitSpeed;
+
+    void Awake()
+    {
+        if(_target == null)
+        {
+            _target = GameObject.Find("Sun").transform;
+        }
+    }
+    void Update()
+    {
+        transform.RotateAround(_target.position, transform.up, _orbitSpeed * Time.deltaTime);
+    }
+}
